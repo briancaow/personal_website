1f6746a293e7fa09c1b56ffb9b3656775b0ffb69
briancaow <briancao02@gmail.com>
2023-08-21 22:13:00 -0700

    Versioning untracked files

--- src/app/components/Section.tsx
+++
@@ -1,8 +0,0 @@
-export default function Section(props: any) {
-  return (
-    <div className="flex flex-col py-3 px-3 m-3 items-center">
-      <h2 className="pb-2">{props.title}</h2>
-      {props.children}
-    </div>
-  );
-}
